1cb516cba3d3f4d40be634d6bc2b42f537fd8892
Daniel Johansson <dannesnake@gmail.com>
2023-09-27 13:04:48 +0200

    Changed the string variable password to int variable pinCode so the user enters a pin code nsted of a password. Created a method for validating the usernames and pin codes. Added a bool variable isRunning and a while loop to keep the the program running as long as isRunning == true.

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -4,22 +4,41 @@
     {
         static void Main(string[] args)
         {
+            bool isRunning = true;
+
             // Greet user at startup
 
             Console.WriteLine("Välkomen till Sparbanken!");
 
-            // Logging in to account
+            while (isRunning)
+            {
+                // Logging in to account
 
-            Console.Write("Ange användarnamn:");
-            string username = Console.ReadLine();
+                Console.Write("Ange användarnamn:");
+                string username = Console.ReadLine().ToUpper();
 
-            Console.Write("Ange lösenord:");
-            string password = Console.ReadLine();
+                Console.Write("Ange lösenord:");
+                int pinCode = Convert.ToInt32(Console.ReadLine()); // Add a try catch later
 
-            // Menu with funktions
+                isRunning = LoggingIn(username, pinCode);
+
+                // Menu with funktions
+            }
         }
 
         // method for logging in
+
+        public static bool LoggingIn(string username, int pinCode)
+        {
+            
+            if ( (username == "DANIEL" && pinCode == 12345) || (username == "TOBIAS" && pinCode == 54321) || (username == "MARKUS" && pinCode == 67890) || (username == "SANDRA" && pinCode == 09876) || (username == "EMMA" && pinCode == 01234) )
+            {
+                return true;
+            }
+
+            else { return false; }
+
+        }
 
         // method for printing user acounts and current balance
 
