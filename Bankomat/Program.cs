523eaf715311bb1f77079c93b080ea7bcb77864e
Daniel Johansson <dannesnake@gmail.com>
2023-09-27 12:13:47 +0200

    Added comments for where to put the different parts of the code. I have writen the code for printing out a greeting at start up and created two string variables for saving user input username and password.

--- Bankomat/Program.cs
+++ Bankomat/Program.cs
@@ -4,7 +4,27 @@
     {
         static void Main(string[] args)
         {
-            
+            // Greet user at startup
+
+            Console.WriteLine("Välkomen till Sparbanken!");
+
+            // Logging in to account
+
+            Console.Write("Ange användarnamn:");
+            string username = Console.ReadLine();
+
+            Console.Write("Ange lösenord:");
+            string password = Console.ReadLine();
+
+            // Menu with funktions
         }
+
+        // method for logging in
+
+        // method for printing user acounts and current balance
+
+        // method for transfering money between acounts
+
+        // method for withdrawing money
     }
 }
